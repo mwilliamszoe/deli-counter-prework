d4883b2db66fd11af4594078022fdf375ad7300b
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-21 21:16:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,41 +1,38 @@
-# Write your code here.
-katzDeliLine = [];
+katz_deli = [] 
 
-def
-line = []
-if persons_in_line.length == 0
-  puts "The line is currently empty"
-elsif 
-end
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
 
-function currentLine(katzDeliLine) {
-    var line = []
-    if (personsInLine.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + katzDeliLine[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
-    }
-}
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
-}
-function nowServing(personsInLine) {
-  if (personsInLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    var name = personsInLine.shift;
-    return "Currently serving " + name + ".";
-  }
-}
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Tom")
+take_a_number(katz_deli, "Alan")
 
-var num = 0
-function takeANumber(arr){
-  num++
-  arr.push(num)
-  return 'Welcome you are number ' + num 
-}
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
+ 
+line(katz_deli)
+
+
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
+
+now_serving(katz_deli)
