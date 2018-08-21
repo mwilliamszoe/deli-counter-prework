ef64fe933b792bc0d2425e3a49aa67b16099d2af
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-21 21:12:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+var katzDeliLine = [];
+
+function currentLine(katzDeliLine) {
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + katzDeliLine[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
+}
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+function nowServing(personsInLine) {
+  if (personsInLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = personsInLine.shift;
+    return "Currently serving " + name + ".";
+  }
+}
+
+var num = 0
+function takeANumber(arr){
+  num++
+  arr.push(num)
+  return 'Welcome you are number ' + num 
+}
